82ebbd596562e77df5e2d5b59b0d5783fe69a96f
bbenyak <bbenyak128@gmail.com>
2022-01-19 16:12:20 +0100

    Practice function calls and variable scope

---
+++ practice.py
@@ -0,0 +1,11 @@
+
+def convert_feet_to_meters(distance):
+    print(distance)
+    return 0.3048*distance
+
+print('I am here')
+distance = 10.0
+print(distance)
+distance = convert_feet_to_meters(200.0)
+print('Distance is', distance, 'meters')
+print('Now I am done')
